e88c97122369589bf8b3e1f954a1cd405bd4a468
Matt Koenig <koenigmattd@gmail.com>
2019-12-25 16:15:06 -0800

    adding root dir

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.listen(process.env.PORT || 3001, () => {
     console.log(`app is running on port ${process.env.PORT}`);
 })
 
+app.get('/', (req, res) => { res.send('it is working! ')})
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
