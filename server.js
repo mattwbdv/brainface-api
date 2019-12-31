c1b26431ef976b05d576ef1032d04df047ca9688
Matt Koenig <koenigmattd@gmail.com>
2019-12-30 21:11:47 -0800

    fixing database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require ('./controllers/image');
 const db = knex({
         client: 'pg',
         connection: {
-          host : 'postgresql-concave-41975',
-          user : '',
-          password : '',
-          database : 'brainface'
+          host : process.env.DATABASE_URL,
+          ssl: true, 
         }
       });
 
